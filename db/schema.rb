639054308070c7285003edc0bfab352af664e013
EC2 Default User <ec2-user@ip-172-31-31-239.ap-southeast-1.compute.internal>
2019-03-22 11:05:06 +0000

    diariesテーブルにUser nicknameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_123241) do
+ActiveRecord::Schema.define(version: 2019_03_22_105350) do
 
   create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "day_image"
     t.text "message"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "User_nickname"
   end
 
   create_table "flowers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
