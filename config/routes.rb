1d2b84d9952f5e205816f1406fd04ee9d99bb74a
EC2 Default User <ec2-user@ip-172-31-31-239.ap-southeast-1.compute.internal>
2019-04-11 00:15:20 +0000

    deviseでusersモデルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   #devise_for :users
   root  'flowers#index'                       #ルートパスの指定
   resources :flowers                     #flowers_controllerに対してのresourcesメソッド
